463d131747bff22c08da0bbeeefb52ee973ce9ca
Anna Lefour <anna.lefour@laposte.net>
2021-02-24 12:42:34 +0100

    It 12 done

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
 require("dotenv").config();
 require("./config/mongodb");
 
-const express = require('express')
-const hbs = require('hbs')
-const app = express()
+const express = require("express");
+const hbs = require("hbs");
+const app = express();
 
-app.use(express.static(__dirname + "/public")) ;
-app.set("views", __dirname + "/views") ;
-app.set("view engine", "hbs") ;
+app.use(express.static(__dirname + "/public"));
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
 hbs.registerPartials(__dirname + "/views/partials");
 
-const users = [{name: "", email: "", favoriteLangage: ""}]
+const users = [
+  { name: "Bouba", email: "bouba@mail.fr", favoriteLangage: "JS" },
+  { name: "Benji", email: "ben@mail.fr", favoriteLangage: "JS" },
+];
 
-const images = ["/img/image-1.jpg", "/img/image-2.jpg", "/img/image-3.jpg"]
+const images = ["/img/image-1.jpg", "/img/image-2.jpg", "/img/image-3.jpg"];
 
 app.get("/", (request, response) => {
-    response.render("home", {images});
+  response.render("home", { images });
 });
 
 app.get("/my-dev-squad", (request, response) => {
-    response.render("allUsers", users);
+  response.render("allUsers", { users } );
 });
 
 app.get("/add-new-ironhacker", (request, response) => {
-    response.render("formUser");
+  response.render("formUser");
 });
 
 app.listen(process.env.PORT, () => {
-    console.log("Ready @ http://localhost:" + process.env.PORT)
-})
+  console.log("Ready @ http://localhost:" + process.env.PORT);
+});
