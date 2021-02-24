b50c22ab96f200f08219e067bfaa9787ff831756
Alexis Verthuy <alexis.verthuy@gmail.com>
2021-02-24 10:43:41 +0100

    Iteration 5 ok

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 require("dotenv").config();
 require("./config/mongodb");
 
+const express = require('express')
+const hbs = require('hbs')
+const app = express()
+
+app.use(express.static(__dirname + "/public")) ;
+app.set("views", __dirname + "/views") ;
+app.set("view engine", "hbs") ;
+hbs.registerPartials(__dirname + "/views/partials");
+
+const users = [{name: "", email: "", favoriteLangage: ""}]
+
+const images = ["/img/image-1.jpg", "/img/image-2.jpg", "/img/image-3.jpg"]
+
+app.listen(process.env.PORT, () => {
+    console.log("Ready @ http://localhost:" + process.env.PORT)
+})
